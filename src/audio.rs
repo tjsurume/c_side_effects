495a3aaf3dd8f27ad5665a2397fc35fd024ca300
tjsurume <tjsurume1009@gmail.com>
2023-04-10 17:50:13 +0900

    :musical_note: 新しいオーディオの制御を追加。 Ready時などに実行する。 既存のオーディオはループのため使わない。

--- src/audio.rs
+++ src/audio.rs
@@ -9,46 +9,55 @@ pub struct InternalAudioPlugin;
 impl Plugin for InternalAudioPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugin(AudioPlugin)
-            .add_system(start_audio.in_schedule(OnExit(MyState::Loading)))
             .add_system(
-                control_flying_sound
-                    .after(set_movement_actions)
-                    .in_set(OnUpdate(MyState::Playing)),
-            );
+                control_scene_sound
+                    .in_set(OnUpdate(MyState::Playing))
+            )
+            ;
     }
 }
 
 #[derive(Resource)]
 struct FlyingAudio(Handle<AudioInstance>);
 
-fn start_audio(mut commands: Commands, audio_assets: Res<AudioAssets>, audio: Res<Audio>) {
-    audio.pause();
-    let handle = audio
-        .play(audio_assets.flying.clone())
-        .looped()
-        .with_volume(0.3)
-        .handle();
-    commands.insert_resource(FlyingAudio(handle));
-}
+#[derive(Resource)]
+struct ItemGotAudio(Handle<AudioInstance>);
 
-fn control_flying_sound(
-    actions: Res<Actions>,
-    audio: Res<FlyingAudio>,
+#[derive(Resource)]
+struct SceneResultAudio(Handle<AudioInstance>);
+
+
+fn control_scene_sound(
+    audio: Res<Audio>,
+    audio_assets: Res<AudioAssets>,
+    mut commands: Commands, 
     mut audio_instances: ResMut<Assets<AudioInstance>>,
-) {
-    if let Some(instance) = audio_instances.get_mut(&audio.0) {
-        match instance.state() {
-            PlaybackState::Paused { .. } => {
-                if actions.player_movement.is_some() {
-                    instance.resume(AudioTween::default());
-                }
+    our_clock: ResMut<OurClock>,
+    mut actions: ResMut<Actions>,
+)
+{
+    if actions.scene_changed != None {
+        
+
+        match actions.scene_changed
+                {
+            Some(MyTimeState::Result)  => {
+                
+                audio.play(audio_assets.scene_result.clone())
+                .with_volume(0.3);
+            },
+            Some(MyTimeState::Playing)  => {
+                audio.play(audio_assets.go.clone())
+                .with_volume(0.3);
+            },
+            Some(MyTimeState::Ready) => {
+                audio.play(audio_assets.ready.clone())
+                .with_volume(0.3);
             }
-            PlaybackState::Playing { .. } => {
-                if actions.player_movement.is_none() {
-                    instance.pause(AudioTween::default());
-                }
-            }
+            None => {},
             _ => {}
         }
+
     }
+    actions.scene_changed = None;
 }
